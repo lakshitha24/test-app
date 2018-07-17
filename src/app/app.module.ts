50d83bee56dfcf25dddbc571cc9a734e07b63f80
lakshitha24 <lakshithajayaweera@gmail.com>
2018-07-17 13:49:03 +0530

    add componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimeLineComponent } from './time-line/time-line.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeLineComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
